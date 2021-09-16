a901b1b15710c897e2e8a5b40f5dd090702d7317
Peniel Cho <pensurf@naver.com>
2021-09-17 01:39:30 +0900

    warning py-3.5

--- components/Warning.vue
+++ components/Warning.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full bg-gradient-to-r from-blue-100 via-blue-50 to-gray-50">
         <div class="max-w-4xl mx-auto flex justify-center">
-            <div class="py-2 md:py-4 text-gray-500 text-xs md:text-sm">⚠️ 아직 완성되지 않았답니다! 10월 1일에 다시 찾아주세요. 😎</div>
+            <div class="py-2 md:py-3.5 text-gray-500 text-xs md:text-sm">⚠️ 아직 완성되지 않았답니다! 10월 1일에 다시 찾아주세요. 😎</div>
         </div>
     </div>
 </template>
