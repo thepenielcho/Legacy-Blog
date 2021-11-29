faa547c60de1bb17dd907ee4e3cc5b0a94b4c5a6
Peniel Cho <penielcho98@aol.com>
2021-11-30 03:13:34 +0900

    image url abs

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -99,12 +99,12 @@ export default {
             hid: 'og:image',
             property: 'og:image',
             // content: this.article.img
-            content: `https://github.com/thepenielcho/Blog/tree/main/assets/resources/${this.article.img}`
+            content: `https://raw.githubusercontent.com/thepenielcho/Blog/main/assets/resources/${this.article.img}`
             },
             {
             hid: 'og:image:secure_url',
             property: 'og:image:secure_url',
-            content: `https://github.com/thepenielcho/Blog/tree/main/assets/resources/${this.article.img}`
+            content: `https://raw.githubusercontent.com/thepenielcho/Blog/main/assets/resources/${this.article.img}`
             },
             {
             hid: 'og:image:alt',
