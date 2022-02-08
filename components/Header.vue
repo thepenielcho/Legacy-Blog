aa040d2ad493c4fe06fb848389efb9bc444b2499
Peniel Cho <penielcho98@aol.com>
2022-02-08 22:25:22 +0900

    routing rel to abs

--- components/Header.vue
+++ components/Header.vue
@@ -12,13 +12,13 @@
                 <ul class="flex space-x-1 text-base items-center">
                     <li>
                         <div class="py-1 px-2 rounded-lg hover:bg-gray-100">
-                            <nuxt-link to="korean" class="text-gray-700 text-sm">한글</nuxt-link>
+                            <nuxt-link :to="{path: '/korean'}" replace class="text-gray-700 text-sm">한글</nuxt-link>
                         </div>
                     </li>
                     <!-- <li class="text-gray-700 mx-auto">|</li> -->
                     <li>
                         <div class="py-1 px-2 rounded-lg hover:bg-gray-100">
-                            <nuxt-link to="english" class="text-gray-700 text-sm">ENG</nuxt-link>
+                            <nuxt-link :to="{path: '/english'}" replace class="text-gray-700 text-sm">ENG</nuxt-link>
                         </div>
                     </li>
                     <li>
